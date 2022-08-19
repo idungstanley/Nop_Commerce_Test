81402c4182858f4607c7062a44968f52dd220b7c
idungstanley <sundaystanley56@gmail.com>
2022-08-19 04:34:13 +0100

    Add fixtures to avoid repetition

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -3,21 +3,21 @@ class LoginPage:
     textbox_username_id = "Email"
     textbox_password_id = "Password"
     button_login_xpath = "//input[@value='Log in']"
-    link_logout_linktext = "Logout"
+    link_logout_link_text = "Logout"
 
     def __init__(self, driver):
         self.driver = driver
 
-    def setUserName(self, username):
+    def set_user_name(self, username):
         self.driver.find_element_by_id(self.textbox_username_id).clear()
         self.driver.find_element_by_id(self.textbox_username_id).send_keys(username)
 
-    def setPassword(self, password):
+    def set_password(self, password):
         self.driver.find_element_by_id(self.textbox_password_id).clear()
         self.driver.find_element_by_id(self.textbox_password_id).send_keys(password)
 
-    def clickLogin(self):
+    def click_login(self):
         self.driver.find_element_by_xpath(self.button_login_xpath).click()
 
-    def clickLogout(self):
-        self.driver.find_element_by_link_text(self.link_logout_linktext).click()
+    def click_logout(self):
+        self.driver.find_element_by_link_text(self.link_logout_link_text).click()
--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,18 +1,31 @@
 import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
-
 class Test_001_Login:
     baseURL = 'https://admin-demo.nopcommerce.com/'
     username = 'admin@yourstore.com'
     password = "admin"
 
-    def test_homePageTitle(self):
-        self.driver = webdriver.chrome()
+    def test_homePageTitle(self, setup):
+        self.driver = setup
         self.driver.get(self.baseURL)
         act_title = self.driver.title
         self.driver.close()
         if act_title == "Your store. Login":
+            assert True
+        else:
+            assert False
+
+    def test_login(self, setup):
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.lp = LoginPage(self.driver)
+        self.lp.set_user_name(self.username)
+        self.lp.set_password(self.password)
+        self.lp.click_login()
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "Dashboard / nopCommerce administration":
             assert True
         else:
             assert False
