df1c3bda9f42e2502d4676c24d29c42fb4bacabd
idungstanley <sundaystanley56@gmail.com>
2022-08-20 09:32:26 +0100

    Add customers class

---
+++ pageObjects/SearchCustomerPage.py
@@ -0,0 +1,10 @@
+class SearchCustomer():
+    # Add customer Page
+    txtEmail_id = "SearchEmail"
+    txtFirstName_id="SearchFirstName"
+    txtLastName_id="SearchLastName"
+    btnSearch_id="search-customers"
+    tblSearchResults_xpath="//table[@role='grid']"
+    table_xpath="//table[@id='customers-grid']"
+    tableRows_xpath="//table[@id='customers-grid']//tbody/tr"
+    tableColumns_xpath="//table[@id='customers-grid']//tbody/tr/td"
