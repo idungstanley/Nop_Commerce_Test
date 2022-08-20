1b5e23f0e7c28080671573c664d80496944cf23e
idungstanley <sundaystanley56@gmail.com>
2022-08-20 10:00:41 +0100

    Add test cases to addCustomer

---
+++ testCases/test_addCustomer.py
@@ -0,0 +1,14 @@
+import pytest
+import time
+from pageObjects.LoginPage import LoginPage
+from pageObjects.AddcustomerPage import AddCustomer
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+import string
+import random
+
+class Test_003_AddCustomer:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
+    logger = LogGen.loggen()
