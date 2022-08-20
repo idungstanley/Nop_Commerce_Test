e1e70ab323d74464eb775a0ead1574c78ecb737d
idungstanley <sundaystanley56@gmail.com>
2022-08-20 09:36:19 +0100

    Add function to searchCustomers email

--- pageObjects/SearchCustomerPage.py
+++ pageObjects/SearchCustomerPage.py
@@ -29,3 +29,16 @@ class SearchCustomer():
 
     def getNoOfRows(self):
         return len(self.driver.find_elements_by_xpath(self.tableRows_xpath))
+
+    def getNoOfColumns(self):
+        return len(self.driver.find_elements_by_xpath(self.tableColumns_xpath))
+
+    def searchCustomerByEmail(self, email):
+        flag = False
+        for r in range(1, self.getNoOfRows() + 1):
+            table = self.driver.find_element_by_xpath(self.table_xpath)
+            emailid = table.find_element_by_xpath("//table[@id='customers-grid']/tbody/tr[" + str(r) + "]/td[2]").text
+            if emailid == email:
+                flag = True
+                break
+        return flag
