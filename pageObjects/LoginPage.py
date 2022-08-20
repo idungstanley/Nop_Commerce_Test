43e51ec709245603848c53d3cdffe44173b13729
idungstanley <sundaystanley56@gmail.com>
2022-08-20 09:11:14 +0100

    Add customed pageObjects

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -3,21 +3,21 @@ class LoginPage:
     textbox_username_id = "Email"
     textbox_password_id = "Password"
     button_login_xpath = "//input[@value='Log in']"
-    link_logout_link_text = "Logout"
+    link_logout_linktext = "Logout"
 
     def __init__(self, driver):
         self.driver = driver
 
-    def set_user_name(self, username):
+    def setUserName(self, username):
         self.driver.find_element_by_id(self.textbox_username_id).clear()
         self.driver.find_element_by_id(self.textbox_username_id).send_keys(username)
 
-    def set_password(self, password):
+    def setPassword(self, password):
         self.driver.find_element_by_id(self.textbox_password_id).clear()
         self.driver.find_element_by_id(self.textbox_password_id).send_keys(password)
 
-    def click_login(self):
+    def clickLogin(self):
         self.driver.find_element_by_xpath(self.button_login_xpath).click()
 
-    def click_logout(self):
-        self.driver.find_element_by_link_text(self.link_logout_link_text).click()
+    def clickLogout(self):
+        self.driver.find_element_by_link_text(self.link_logout_linktext).click()
